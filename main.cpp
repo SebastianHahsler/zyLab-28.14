a9daf316d95832e1716ed09c108ab8183f05b796
SebastianHahsler <167276710+SebastianHahsler@users.noreply.github.com>
2024-04-30 11:50:10 -0500

    Update main.cpp
    Finishing the last part of main

--- main.cpp
+++ main.cpp
@@ -47,3 +47,78 @@ int main() {
 
    return 0;
 }
+void PrintMenu() { 
+    cout << "\nMENU" << endl;
+    cout << "a - Add item to cart" << endl;
+    cout << "d - Remove item from cart" << endl;
+    cout << "c - Change item quantity" << endl;
+    cout << "i - Output items' descriptions" << endl;
+    cout << "o - Output shopping cart" << endl;
+    cout << "q - Quit\n" << endl;
+    //cout << "\nChoose an option:" << endl; //No end Line
+}
+
+char GetMenuOption() {
+   cout << "Choose an option:" << endl; //No end Line
+    char option;
+    cin >> option;
+    cin.ignore(); // Clear the newline character or any extra input
+    return option;
+}
+
+void ExecuteMenu(char option, ShoppingCart& cart) {
+    string name, description;
+    int price, quantity;
+
+    switch (option) {
+        case 'a': {
+            cout << "\nADD ITEM TO CART" << endl;
+            cout << "Enter the item name:" << endl;
+            getline(cin, name);
+            cout << "Enter the item description:" << endl;
+            getline(cin, description);
+            cout << "Enter the item price:" << endl;
+            cin >> price;
+            cout << "Enter the item quantity:" << endl;
+            cin >> quantity;
+            cin.ignore(); // Clear newline character after numeric input
+
+            ItemToPurchase newItem(name, description, price, quantity);
+            cart.AddItem(newItem);
+            break;
+        }
+        case 'd': {
+            cout << "\nREMOVE ITEM FROM CART" << endl;
+            cout << "Enter name of item to remove:" << endl;
+            getline(cin, name);
+            cart.RemoveItem(name);
+            break;
+        }
+        case 'c': {
+            cout << "\nCHANGE ITEM QUANTITY" << endl;
+            cout << "Enter the item name:" << endl;
+            getline(cin, name);
+            cout << "Enter the new quantity:" << endl;
+            cin >> quantity;
+            cin.ignore(); // Clear newline character after numeric input
+
+            ItemToPurchase modItem;
+            modItem.SetName(name);
+            modItem.SetQuantity(quantity);
+            cart.ModifyItem(modItem);
+            break;
+        }
+        case 'i':
+            cout << "\nOUTPUT ITEMS' DESCRIPTIONS" << endl;
+            cart.PrintDescriptions();
+            break;
+        case 'o':
+            cout << "OUTPUT SHOPPING CART" << endl;
+            cart.PrintTotal();
+            break;
+        case 'q':
+            break;
+        default:
+            break;
+    }
+}
