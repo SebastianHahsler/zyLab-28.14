0e6fbf480bcbbde9018cc368d0503f151fdeb13c
Tierneygc <167291547+Tierneygc@users.noreply.github.com>
2024-04-30 11:45:37 -0500

    Update main.cpp
    Updated main function.

--- main.cpp
+++ main.cpp
@@ -13,9 +13,37 @@ void ExecuteMenu(char option, ShoppingCart& theCart) {
    /* Type your code here */
    
 }
-
+char GetMenuOption() {
+    
+}
 int main() {
-   /* Type your code here */
+   string customerName;
+    string date;
    
+    cout << "Enter customer's name:" << endl;
+    getline(cin, customerName);
+    cout << "Enter today's date:" << endl;
+    getline(cin, date);
+
+    ShoppingCart shoppingCart(customerName, date);
+
+    cout << "\nCustomer name: " << customerName << endl;
+    cout << "Today's date: " << date << endl;
+ 
+   PrintMenu(); 
+
+    char menuOption = ' ';
+    menuOption = GetMenuOption();
+    do {
+      if (menuOption == 'a' || menuOption == 'd' || menuOption == 'c' || menuOption == 'i' || menuOption == 'o'){
+         ExecuteMenu(menuOption, shoppingCart);
+         PrintMenu();
+         menuOption = GetMenuOption();
+      }
+      else{
+         menuOption = GetMenuOption();
+      }
+    } while (menuOption != 'q');
+
    return 0;
 }
